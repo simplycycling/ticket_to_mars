95cdaa7377f539090d7e2244486922afd78d967e
Roger Sherman <rsherman@viddler.com>
2016-12-13 18:34:31 +1100

    Cleaning things up a little, putting in empty company variable

--- ticket_to_mars.go
+++ ticket_to_mars.go
@@ -6,25 +6,16 @@ import (
 )
 
 func main() {
-	distance := 56000000
+	distance := 57600000
+	company := ""
 	speed := rand.Intn(16) + 15
 	days := (distance / speed) / 86164
-	//days := rand.Intn(16) + 15
-	//price := days / 2
+	
 	fmt.Printf("%-18v %4v %v %7v\n", "Spaceline", "Days", "Round-trip", "Price")
 	fmt.Println("===============================================")
 	fmt.Println(distance / speed, days)
-	/*spaceline := {
-		switch rand.Intn(2) {
-		case 0:
-			"Virgin Galactic"
-		case 1:
-			"SpaceX"
-		case 2:
-			"Space Adventures"
-		}
-	}*/
-	/*for count :=0; count < 10; count++ {
-		fmt.Printf("%-18v %4v %v $%4v\n", spaceline, days, "Round trip", 22)
-	}*/
+	for count :=0; count < 10; count++ {
+		
+		fmt.Printf("%-18v %4v %v $%4v\n", company, days, "Round trip", 22)
+	}
 }
