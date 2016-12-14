f494d27ffce2576d7a1ed249b5a14b144afaf4b1
Roger Sherman <rsherman@viddler.com>
2016-12-14 11:06:19 +1100

    Put speed and days variables into the proper scope, added price variable

--- ticket_to_mars.go
+++ ticket_to_mars.go
@@ -8,14 +8,23 @@ import (
 func main() {
 	distance := 57600000
 	company := ""
-	speed := rand.Intn(16) + 15
-	days := (distance / speed) / 86164
 	
 	fmt.Printf("%-18v %4v %v %7v\n", "Spaceline", "Days", "Round-trip", "Price")
 	fmt.Println("===============================================")
-	fmt.Println(distance / speed, days)
+	//fmt.Println(distance / speed, days)
 	for count :=0; count < 10; count++ {
+		switch rand.Intn(3) {
+		case 0:
+			company = "Space Adventures"
+		case 1:
+			company = "SpaceX"
+		case 2:
+			company = "Virgin Galactic"
+		}
+		speed := rand.Intn(16) + 15
+		days := (distance / speed) / 86164
+		price := days / 2
 		
-		fmt.Printf("%-18v %4v %v $%4v\n", company, days, "Round trip", 22)
+		fmt.Printf("%-18v %4v %v $%4v\n", company, days, "Round trip", price)
 	}
 }
